d2e9c4ccd039baf06bd4e5fa528ee7307dd04585
oleh19127 <oleh19127@gmail.com>
2024-06-25 12:19:37 +0300

    part-3

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -6,3 +6,10 @@ DATABASES = {
  'PASSWORD': 'mysite_pass',
  }
 }
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
